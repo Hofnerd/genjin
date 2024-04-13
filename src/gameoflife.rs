e008af1a9bb362f4380f70871a72b65e295c2306
Spencer Lombardo <spencerlombardo96@gmail.com>
2024-04-13 17:51:10 -0400

    Initial code. Followed game of life example of sdl2 rust examples

---
+++ src/gameoflife.rs
@@ -0,0 +1,135 @@
+use crate::GAME_FIELD_HEIGHT;
+use crate::GAME_FIELD_WIDTH;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum GameState {
+    Playing,
+    Paused,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CellState {
+    Alive,
+    Dead,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Cell {
+    pub posx: i32,
+    pub posy: i32,
+    pub state: CellState,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct GameOfLife {
+    pub state: GameState,
+    field: [Cell; (GAME_FIELD_WIDTH * GAME_FIELD_HEIGHT) as usize],
+}
+
+impl GameOfLife {
+    pub fn new() -> GameOfLife {
+        let mut pg =
+            [Cell::new(0, 0, CellState::Dead); (GAME_FIELD_WIDTH * GAME_FIELD_HEIGHT) as usize];
+        for i in 0..GAME_FIELD_HEIGHT {
+            for j in 0..GAME_FIELD_WIDTH {
+                pg[(j as u32 + ((i as u32) * GAME_FIELD_WIDTH)) as usize] =
+                    Cell::new(i as i32, j as i32, CellState::Dead);
+            }
+        }
+
+        GameOfLife {
+            state: GameState::Paused,
+            field: pg,
+        }
+    }
+
+    pub fn get(&self, x: i32, y: i32) -> Option<Cell> {
+        if x >= 0 && y >= 0 && (x as u32) < GAME_FIELD_WIDTH && (y as u32) < GAME_FIELD_HEIGHT {
+            return Some(self.field[(x as u32 + (y as u32) * GAME_FIELD_WIDTH) as usize]);
+        } else {
+            return None;
+        }
+    }
+
+    pub fn get_mut(&mut self, x: i32, y: i32) -> Option<&mut Cell> {
+        if x >= 0 && y >= 0 && (x as u32) < GAME_FIELD_WIDTH && (y as u32) < GAME_FIELD_HEIGHT {
+            return Some(&mut self.field[(x as u32 + (y as u32) * GAME_FIELD_WIDTH) as usize]);
+        } else {
+            return None;
+        }
+    }
+
+    pub fn toggle_state(&mut self) {
+        self.state = match self.state {
+            GameState::Paused => GameState::Playing,
+            GameState::Playing => GameState::Paused,
+        }
+    }
+
+    pub fn update(&mut self) {
+        match self.state {
+            GameState::Paused => {}
+            GameState::Playing => {
+                let mut tmp_field = self.field;
+                for (u, cell) in tmp_field.iter_mut().enumerate() {
+                    let u = u as u32;
+                    let x = u % GAME_FIELD_WIDTH;
+                    let y = u / GAME_FIELD_WIDTH;
+
+                    let mut count: u32 = 0;
+                    for i in -1..2 {
+                        for j in -1..2 {
+                            if !(i == 0 && j == 0) {
+                                let px: i32 = (x as i32) + i;
+                                let py: i32 = (y as i32) + j;
+
+                                let c = self
+                                    .get(px, py)
+                                    .unwrap_or_else(|| Cell::new(0, 0, CellState::Dead));
+
+                                if &c != cell {
+                                    if c.state == CellState::Alive {
+                                        count += 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if count > 3 || count < 2 {
+                        cell.state = CellState::Dead;
+                    } else if count == 3 {
+                        cell.state = CellState::Alive;
+                    } else if count == 2 {
+                        cell.state = cell.state;
+                    }
+                }
+                self.field = tmp_field;
+            }
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a GameOfLife {
+    type Item = &'a Cell;
+    type IntoIter = ::std::slice::Iter<'a, Cell>;
+    fn into_iter(self) -> ::std::slice::Iter<'a, Cell> {
+        return self.field.iter();
+    }
+}
+
+impl Cell {
+    pub fn new(x: i32, y: i32, state: CellState) -> Cell {
+        return Cell {
+            posx: x,
+            posy: y,
+            state: state,
+        };
+    }
+
+    pub fn toggle_state(&mut self) {
+        self.state = match self.state {
+            CellState::Dead => CellState::Alive,
+            CellState::Alive => CellState::Dead,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,203 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::video::{Window, WindowContext};
+
+use crate::gameoflife::CellState;
+
+mod gameoflife;
+
+pub const SQUARE_SIZE: u32 = 40;
+pub const GAME_FIELD_WIDTH: u32 = 40;
+pub const GAME_FIELD_HEIGHT: u32 = 40;
+
+fn dummy_texture<'a>(
+    canvas: &mut Canvas<Window>,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> Result<(Texture<'a>, Texture<'a>), String> {
+    enum TextureColor {
+        Yellow,
+        White,
+    };
+    let mut st1 = texture_creator
+        .create_texture_target(None, SQUARE_SIZE, SQUARE_SIZE)
+        .map_err(|e| e.to_string())?;
+
+    let mut st2 = texture_creator
+        .create_texture_target(None, SQUARE_SIZE, SQUARE_SIZE)
+        .map_err(|e| e.to_string())?;
+    {
+        let textures = vec![
+            (&mut st1, TextureColor::Yellow),
+            (&mut st2, TextureColor::White),
+        ];
+
+        canvas
+            .with_multiple_texture_canvas(textures.iter(), |texture_canvas, user_context| {
+                texture_canvas.set_draw_color(Color::RGB(0, 0, 0));
+                texture_canvas.clear();
+                match *user_context {
+                    TextureColor::Yellow => {
+                        for i in 0..SQUARE_SIZE {
+                            for j in 0..SQUARE_SIZE {
+                                if (i + j) % 4 == 0 {
+                                    texture_canvas.set_draw_color(Color::RGB(255, 255, 0));
+                                    texture_canvas
+                                        .draw_point(Point::new(i as i32, j as i32))
+                                        .expect("could not draw point");
+                                }
+                                if (i + j) % 9 == 0 {
+                                    texture_canvas.set_draw_color(Color::RGB(200, 200, 0));
+                                    texture_canvas
+                                        .draw_point(Point::new(i as i32, j as i32))
+                                        .expect("could not draw point");
+                                }
+                            }
+                        }
+                    }
+                    TextureColor::White => {
+                        for i in 0..SQUARE_SIZE {
+                            for j in 0..SQUARE_SIZE {
+                                if (i + j) % 7 == 0 {
+                                    texture_canvas.set_draw_color(Color::RGB(192, 192, 192));
+                                    texture_canvas
+                                        .draw_point(Point::new(i as i32, j as i32))
+                                        .expect("could not draw point");
+                                }
+                                if (i + j) % 5 == 0 {
+                                    texture_canvas.set_draw_color(Color::RGB(64, 64, 64));
+                                    texture_canvas
+                                        .draw_point(Point::new(i as i32, j as i32))
+                                        .expect("could not draw point");
+                                }
+                            }
+                        }
+                    }
+                };
+                for i in 0..SQUARE_SIZE {
+                    for j in 0..SQUARE_SIZE {
+                        if (i + j) % 7 == 0 {
+                            texture_canvas.set_draw_color(Color::RGB(192, 192, 192));
+                            texture_canvas
+                                .draw_point(Point::new(i as i32, j as i32))
+                                .expect("could not draw point");
+                        }
+                        if (i + j) % 5 == 0 {
+                            texture_canvas.set_draw_color(Color::RGB(64, 64, 64));
+                            texture_canvas
+                                .draw_point(Point::new(i as i32, j as i32))
+                                .expect("could not draw point");
+                        }
+                    }
+                }
+            })
+            .map_err(|e| e.to_string())?;
+    }
+
+    return Ok((st1, st2));
+}
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window(
+            "Rust SDL2 Demo: Game of life",
+            SQUARE_SIZE * GAME_FIELD_WIDTH,
+            SQUARE_SIZE * GAME_FIELD_HEIGHT,
+        )
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .target_texture()
+        .present_vsync()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    println!("Using SDL_Render \"{}\"", canvas.info().name);
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let tc: TextureCreator<_> = canvas.texture_creator();
+
+    let (st1, st2) = dummy_texture(&mut canvas, &tc)?;
+
+    let mut game = gameoflife::GameOfLife::new();
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let mut frame: u32 = 0;
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Space),
+                    repeat: false,
+                    ..
+                } => {
+                    game.toggle_state();
+                }
+                Event::MouseButtonDown {
+                    x,
+                    y,
+                    mouse_btn: MouseButton::Left,
+                    ..
+                } => {
+                    let x = (x as u32) / SQUARE_SIZE;
+                    let y = (y as u32) / SQUARE_SIZE;
+                    match game.get_mut(x as i32, y as i32) {
+                        Some(cell) => {
+                            cell.toggle_state();
+                        }
+                        None => unreachable!(),
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        if frame >= 30 {
+            game.update();
+            frame = 0;
+        }
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        for (i, cell) in (&game).into_iter().enumerate() {
+            let i = i as u32;
+            let st = if frame >= 15 { &st1 } else { &st2 };
+            if cell.state == CellState::Alive {
+                canvas.copy(
+                    st,
+                    None,
+                    Rect::new(
+                        ((i % GAME_FIELD_WIDTH) * SQUARE_SIZE) as i32,
+                        ((i / GAME_FIELD_WIDTH) * SQUARE_SIZE) as i32,
+                        SQUARE_SIZE,
+                        SQUARE_SIZE,
+                    ),
+                )?;
+            }
+        }
+        canvas.present();
+        if let gameoflife::GameState::Playing = game.state {
+            frame += 1;
+        }
+    }
+
+    return Ok(());
+}
